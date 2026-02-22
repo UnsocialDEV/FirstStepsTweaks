a80b1f0737a093d32ad58fb9f715a397ed98a2d3
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2026-02-22 14:16:07 -0700

    fixed bugs

--- FirstStepsTweaks/Commands/ChiselTemplateCommands.cs
+++ FirstStepsTweaks/Commands/ChiselTemplateCommands.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using Newtonsoft.Json.Linq;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
