d83952e0a811f1389d8f887992a4b9c36fe219ba
DaytonJWatson <45436514+DaytonJWatson@users.noreply.github.com>
2026-03-02 12:19:09 -0700

    Revert "Add chisel export/import command framework with serialization and file storage"

--- FirstStepsTweaks/Config/ChiselTransferConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace FirstStepsTweaks.Config
-{
-    public class ChiselTransferConfig
-    {
-        public int MaxBlocksPerExport { get; set; } = 32768;
-        public bool AllowOverwriteOnImportDefault { get; set; } = false;
-        public string ExportFolderName { get; set; } = "Exported";
-    }
-}
